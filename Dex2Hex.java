5ae7b683ea54651467fc7b8b433fb0b7413f1e10
jackbruce318 <jackbruce522@gmail.com>
2024-11-05 16:01:09 +0000

    fixed argument assignment and added string input error handling

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
-//altering comment here to test Jenkins automation
+//A program which converts a decimal number to a hexadecimal number
 class Dec2Hex
 {
 public static int Arg1;
     public static void main(String args[])    {
+        //if no arguments are provided then this handles the error
         if (args.length == 0) {
-            System.out.println("Please enter a valid number");
+            System.out.println("Please enter a valid decimal number");
             return;
         }
-
-        try {
-            // Attempt to pass Arg1 as an integer
-            Arg1 = Integer.parseInt(args[0]);
-        } 
+        try{
+        Arg1 = Integer.parseInt(args[0]);
+        }
         catch (NumberFormatException e) {
-            System.out.println("Please enter a valid number");
+            System.out.println("Please enter a valid decimal number");
             return;
         }
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+       
+            char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
         String hexadecimal="";
@@ -32,6 +32,10 @@ public static int Arg1;
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+         
+        
+       
 
     }
 }
